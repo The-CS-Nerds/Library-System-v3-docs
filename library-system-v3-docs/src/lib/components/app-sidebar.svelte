31f1b2bcfdefbe2a4e995fe9204d828de9f6e703
SuitablyMysterious <fly2be@outlook.com>
2025-11-08 10:19:56 +0000

    .

---
+++ library-system-v3-docs/src/lib/components/app-sidebar.svelte
@@ -0,0 +1,70 @@
+<script lang="ts" module>
+	// sample data
+	const data = {
+		navMain: [
+			{
+				title: "Welcome",
+				url: "#",
+				items: [
+					{
+						title: "Overview",
+						url: "#",
+						isActive: false
+					}
+				]
+			},
+			{
+				title: "Getting Started",
+				url: "#",
+				items: [
+					{
+						title: "Installation",
+						url: "#",
+						isActive: false,
+					},
+					{
+						title: "Project Structure",
+						url: "#",
+						isActive: false,
+					},
+				],
+			},
+		],
+	};
+</script>
+
+<script lang="ts">
+	import SearchForm from "./search-form.svelte";
+	import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+	import type { ComponentProps } from "svelte";
+
+	let { ref = $bindable(null), ...restProps }: ComponentProps<typeof Sidebar.Root> = $props();
+</script>
+
+<Sidebar.Root {...restProps} bind:ref>
+	<Sidebar.Header>
+		<SearchForm />
+	</Sidebar.Header>
+	<Sidebar.Content>
+		<!-- We create a Sidebar.Group for each parent. -->
+		{#each data.navMain as group (group.title)}
+			<Sidebar.Group>
+				<Sidebar.GroupLabel>{group.title}</Sidebar.GroupLabel>
+				<Sidebar.GroupContent>
+					<Sidebar.Menu>
+						{#each group.items as item (item.title)}
+							<Sidebar.MenuItem>
+								<Sidebar.MenuButton isActive={item.isActive}>
+									{#snippet child({ props })}
+										<a href={item.url} {...props}>{item.title}</a>
+									{/snippet}
+								</Sidebar.MenuButton>
+							</Sidebar.MenuItem>
+						{/each}
+					</Sidebar.Menu>
+				</Sidebar.GroupContent>
+			</Sidebar.Group>
+		{/each}
+	</Sidebar.Content>
+	<Sidebar.Rail />
+</Sidebar.Root>
---
+++ library-system-v3-docs/src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import AppSidebar from "$lib/components/app-sidebar.svelte";
+	import * as Breadcrumb from "$lib/components/ui/breadcrumb/index";
+	import { Separator } from "$lib/components/ui/separator/index";
+	import * as Sidebar from "$lib/components/ui/sidebar/index";
+</script>
+
+<Sidebar.Provider>
+	<AppSidebar />
+	<Sidebar.Inset>
+		<header class="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+			<Sidebar.Trigger class="-ml-1" />
+			<Separator orientation="vertical" class="mr-2 h-4" />
+			<Breadcrumb.Root>
+				<Breadcrumb.List>
+					<Breadcrumb.Separator class="hidden md:block" />
+					<Breadcrumb.Item>
+						<Breadcrumb.Page>Overview</Breadcrumb.Page>
+					</Breadcrumb.Item>
+				</Breadcrumb.List>
+			</Breadcrumb.Root>
+		</header>
+	</Sidebar.Inset>
+</Sidebar.Provider>
